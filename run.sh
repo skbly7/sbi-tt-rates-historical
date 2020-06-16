cea49c30e3f419d4b405ef289e772db8ad445052
Shivam Khandelwal (auto commit) <noreply@shivamkhandelwal.in>
2020-06-17 03:27:04 +0530

    Authorship moved to noreply email to save Github profile being polluted

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 wget https://www.sbi.co.in/documents/16012/1400784/FOREX_CARD_RATES.pdf -O ~/sbi-tt-rates-historical/$(date '+%Y-%m-%d-%H:%M').pdf
 git add .
+git config user.email "noreply@shivamkhandelwal.in"
+git config user.name "Shivam Khandelwal (auto commit)"
 git commit -m "Daily update $(date '+%Y-%m-%d-%H:%M')"
 git push origin master

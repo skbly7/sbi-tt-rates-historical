6ca51db6fab6253c35f22849526c8908b217c6a3
Shivam Khandelwal (auto commit) <noreply@shivamkhandelwal.in>
2021-11-25 21:10:08 +0530

    Daily update 2021-11-25-21:10

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
-wget https://www.sbi.co.in/documents/16012/1400784/FOREX_CARD_RATES.pdf -O ~/sbi-tt-rates-historical/$(date '+%Y-%m-%d-%H:%M').pdf
+sudo service tor restart
+#sleep 5
+#curl --socks5 localhost:9050 https://www.sbi.co.in/documents/16012/1400784/FOREX_CARD_RATES.pdf --output ~/sbi-tt-rates-historical/$(date '+%Y-%m-%d-%H:%M').pdf
+curl https://www.sbi.co.in/documents/16012/1400784/FOREX_CARD_RATES.pdf --output ~/sbi-tt-rates-historical/$(date '+%Y-%m-%d-%H:%M').pdf
 git add .
 git config user.email "noreply@shivamkhandelwal.in"
 git config user.name "Shivam Khandelwal (auto commit)"

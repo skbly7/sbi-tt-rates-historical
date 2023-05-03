daf00ce3b298619b257f8340c0e442134a64aa4b
Shivam Khandelwal (auto commit) <noreply@shivamkhandelwal.in>
2023-05-03 20:20:53 +0000

    curl --create-dirs

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #sudo service tor restart
 #sleep 5
 #curl --socks5 localhost:9050 https://www.sbi.co.in/documents/16012/1400784/FOREX_CARD_RATES.pdf --output ~/sbi-tt-rates-historical/$(date '+%Y-%m-%d-%H:%M').pdf
-curl https://www.sbi.co.in/documents/16012/1400784/FOREX_CARD_RATES.pdf --output ~/sbi-tt-rates-historical/$(date '+%Y')/$(date '+%m')/$(date '+%Y-%m-%d-%H:%M').pdf
+curl --create-dirs https://www.sbi.co.in/documents/16012/1400784/FOREX_CARD_RATES.pdf --output ~/sbi-tt-rates-historical/$(date '+%Y')/$(date '+%m')/$(date '+%Y-%m-%d-%H:%M').pdf
 git add .
 git config user.email "noreply@shivamkhandelwal.in"
 git config user.name "Shivam Khandelwal (auto commit)"

7558f816fe282fc35f4cf1bca4e697b46905ebc6
Shivam Khandelwal (auto commit) <noreply@shivamkhandelwal.in>
2025-09-26 20:38:57 +0000

    SBI url changed

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #sudo service tor restart
 #sleep 5
 #curl --socks5 localhost:9050 --create-dirs https://www.sbi.co.in/documents/16012/1400784/FOREX_CARD_RATES.pdf --output ~/sbi-tt-rates-historical/$(date '+%Y')/$(date '+%m')/$(date '+%Y-%m-%d-%H:%M').pdf
-~/curl-impersonate/curl-impersonate-chrome -L --create-dirs https://www.sbi.co.in/documents/16012/1400784/FOREX_CARD_RATES.pdf --output ~/sbi-tt-rates-historical/$(date '+%Y')/$(date '+%m')/$(date '+%Y-%m-%d-%H:%M').pdf
+~/curl-impersonate/curl-impersonate-chrome -L --create-dirs https://sbi.bank.in/documents/16012/1400784/FOREX_CARD_RATES.pdf --output ~/sbi-tt-rates-historical/$(date '+%Y')/$(date '+%m')/$(date '+%Y-%m-%d-%H:%M').pdf
 git add .
 git config user.email "noreply@shivamkhandelwal.in"
 git config user.name "Shivam Khandelwal (auto commit)"
